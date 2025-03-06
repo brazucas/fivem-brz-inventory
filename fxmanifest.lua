a39750136866742973fdc6b3857138701fa6be70
pedropapa <pedro.papadopolis@gmail.com>
2025-03-06 15:53:55 -0300

    chore: add fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+name 'brz-inventory'
+author 'brz.gg'
+lua54 'yes'
+game 'gta5'
+
+shared_scripts {
+    'settings.js',
+    '@ox_lib/init.lua',
+}
+
+ox_libs {
+    'interface',
+}
+
+server_script 'dist/server/**/*.js'
+
+client_script 'dist/client/**/*.js'
+
+ui_page 'nui/inventory.html'
+
+files {
+    'settings.js',
+    'items.js',
+    'nui/*.html',
+    'nui/dist/*.js',
+    'nui/dist/dom/*.js'
+}
