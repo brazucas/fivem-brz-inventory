9f32fea2d52a84f35d108bb9c2ded44d6f3b4cfe
pedropapa <pedro.papadopolis@gmail.com>
2025-03-08 17:05:18 -0300

    refactor: extract validations

--- server/commands.ts
+++ server/commands.ts
@@ -14,31 +14,13 @@ import { notify } from "@core/notification";
 import { randomUUID } from "crypto";
 
 export const givePlayerItemCommand = async (source: number, args: string[]) => {
-  if (args.length < 3) {
-    notify(
-      source,
-      `Invalid number of arguments, expected: 3, received: ${args.length}`,
-      "error"
-    );
+  const attributes = validateInventoryItemCommand(source, args);
+
+  if (!attributes) {
     return;
   }
 
-  const playerId = String(args[0]);
-  const itemId = String(args[1]) as ItemId;
-  const quantity = parseInt(args[2]) as Quantity;
-
-  if (!isPlayerConnected(playerId)) {
-    notify(source, `Player id ${playerId} is not connected`, "error");
-    return;
-  }
-
-  const playerName = GetPlayerName(playerId);
-  const inventoryItem = getItem(itemId);
-
-  if (!inventoryItem) {
-    notify(source, `The item ${itemId} doesn't exists`, "error");
-    return;
-  }
+  const { playerId, itemId, quantity, playerName } = attributes;
 
   try {
     await createInventoryItem({
@@ -75,6 +57,37 @@ export const removePlayerItemCommand = async (
   source: number,
   args: string[]
 ) => {
+  const attributes = validateInventoryItemCommand(source, args);
+
+  if (!attributes) {
+    return;
+  }
+
+  const { itemId, quantity, playerName } = attributes;
+
+  try {
+    await removeInventoryItem(
+      `player_${playerName}` as InventoryId,
+      itemId,
+      quantity
+    );
+
+    notify(
+      source,
+      `Removed item ${itemId} (${quantity}x) from ${playerName}`,
+      "success"
+    );
+  } catch (err) {
+    console.error(err);
+    notify(
+      source,
+      `An error occurred while removing item ${itemId} from ${playerName}`,
+      "error"
+    );
+  }
+};
+
+const validateInventoryItemCommand = (source: number, args: string[]) => {
   if (args.length < 3) {
     notify(
       source,
@@ -101,26 +114,7 @@ export const removePlayerItemCommand = async (
     return;
   }
 
-  try {
-    await removeInventoryItem(
-      `player_${playerName}` as InventoryId,
-      itemId,
-      quantity
-    );
-
-    notify(
-      source,
-      `Removed item ${itemId} (${quantity}x) from ${playerName}`,
-      "success"
-    );
-  } catch (err) {
-    console.error(err);
-    notify(
-      source,
-      `An error occurred while removing item ${itemId} from ${playerName}`,
-      "error"
-    );
-  }
+  return { playerId, itemId, quantity, playerName };
 };
 
 RegisterCommand("givePlayerItem", givePlayerItemCommand, false);
