1f8bd48bb835afc613351b29ed76ae01a466aab9
pedropapa <pedro.papadopolis@gmail.com>
2025-03-10 12:32:56 +1100

    fix(client): add commands import

--- client/client.ts
+++ client/client.ts
@@ -1,5 +1,6 @@
 import { OrderedInventoryIndex } from "@common/types";
 import { setState } from "./inventory-state";
+import "./commands";
 
 onNet("brz-inventory:setState", (inventory: OrderedInventoryIndex) =>
   setState(inventory)
