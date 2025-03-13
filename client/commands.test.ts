275c404502dd925852e30e93b30e313cb8f3ba36
pedropapa <pedro.papadopolis@gmail.com>
2025-03-13 20:57:40 +1100

    feat(client): add item name when printing inventory

--- client/commands.test.ts
+++ client/commands.test.ts
@@ -1,5 +1,38 @@
 global.RegisterCommand = jest.fn();
 global.emit = jest.fn();
+(global as any).ITEMS = [
+  {
+    id: "crowbar",
+    name: "Crowbar",
+    type: "weapon",
+    description:
+      "A trusty tool for prying things open, or for when you just need to make a point.",
+    tier: 1,
+    rarity: "common",
+    weight: 2000,
+    stackable: false,
+    decayable: false,
+    droppable: true,
+    usable: true,
+    onUseHandler: "useCrowbar",
+    tradable: true,
+  },
+  {
+    id: "hat",
+    name: "Hat",
+    type: "weapon",
+    description: "Description 2",
+    tier: 1,
+    rarity: "common",
+    weight: 2000,
+    stackable: false,
+    decayable: false,
+    droppable: true,
+    usable: true,
+    onUseHandler: "useItem2",
+    tradable: true,
+  },
+];
 
 import { printInventoryCommand } from "./commands";
 import { getState } from "./inventory-state";
@@ -14,15 +47,15 @@ describe("commands", () => {
       const consoleLogSpy = jest.spyOn(console, "log");
 
       (getState as jest.Mock).mockReturnValue({
-        "1": { id: "1", quantity: 1 },
-        "2": { id: "2", quantity: 2 },
+        "1": { id: "1", quantity: 1, itemId: "crowbar" },
+        "2": { id: "2", quantity: 2, itemId: "hat" },
       });
 
       await printInventoryCommand(1);
 
       expect(consoleLogSpy).toHaveBeenCalledWith(
         "formattedMessage",
-        "Item 1: 1x\nItem 2: 2x"
+        "Item Crowbar: 1x\nItem Hat: 2x"
       );
     });
   });
--- client/commands.ts
+++ client/commands.ts
@@ -1,10 +1,14 @@
+import { getItemInfo } from "@common/helpers";
 import { getState } from "./inventory-state";
 
 export const printInventoryCommand = async (source: number) => {
   const inventoryState = getState();
 
   const formattedMessage = Object.entries(inventoryState)
-    .map(([itemId, { quantity }]) => `Item ${itemId}: ${quantity}x`)
+    .map(([_, { quantity, itemId }]) => {
+      const itemInfo = getItemInfo(itemId);
+      return `Item ${itemInfo?.name || itemId}: ${quantity}x`;
+    })
     .join("\n");
 
   console.log("formattedMessage", formattedMessage);
